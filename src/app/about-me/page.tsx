9095a3467f62ff4ce3ae6839cfc1278ffa4c6cae
Kyle Armstrong <kylearmstrong96@outlook.com>
2024-01-01 14:43:57 -0500

    Modified for mobile devices

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -13,20 +13,20 @@ function CenterSection() {
   return (
     <div>
       <div className="flex justify-center py-[200px]">
-        <div className="flex justify-center py-5">
+        <div className="relative min-w-[192px] min-h-[192px] max-h-[384px] max-w-[384px] py-5">
           <SelfImage />
         </div>
         <div className="px-5 flex flex-col justify-center justify-items-center dark:text-slate-300">
-          <div className="text-5xl text-right flex justify-start">
+          <div className="text-5xl sm:text-2xl md:text-3xl lg:text-4xl flex justify-start">
             <h1 className="font-semibold">Programmer, tinkerer, hobbyist. </h1>
           </div>
-          <div className="flex justify-start text-right">
-            <h2 className="font-medium text-xl">
+          <div className="flex justify-start text-left">
+            <h2 className="font-medium text-xl sm:text-md md:text-md lg:text-md ">
               I finally got around to building this site...
             </h2>
           </div>
           <div className="py-6 flex justify-start max-w-[700px]">
-            <div className="text-md">
+            <div className="text-md sm:text-md md:text-md lg:text-md ">
               <p>
                 Hello! I&apos;m a full-stack software engineer who enjoys
                 everything technology, listenting to IDM (on rare occassions
@@ -35,9 +35,9 @@ function CenterSection() {
                 this website over the coming years with whatever work I&apos;ve
                 done and think is worth sharing. I am currently in the process of
                 filling out my &apos;Projects&apos; scetion with current projects I&apos;ve
-                worked on.
+                worked on and hope to have it done soon.
               </p>
-              <br />
+              <br/>
               <p>
                 Feel free to check out my Github, LinkedIn, and Email down below
                 if you&apos;d like to look at any of my work or get in touch.
@@ -54,9 +54,15 @@ function SelfImage() {
   return (
     <Image
       src="/me.jpg"
-      alt="Me"
-      width="384"
-      height="384"
+      alt="A picture of me"
+      sizes="100vw"
+      style={{
+        width: '100%',
+        height: 'auto',
+      }}
+      width={500}
+      height={300}
+      placeholder = 'empty'
       className="rounded-[50%] shadow-md"
     />
   );
